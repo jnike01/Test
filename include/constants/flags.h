17784147ad3b91ea158da850556c2ed10fb6a8e2
jnike01 <bekennedy09@gmail.com>
2024-03-15 13:04:43 -0400

    made new map and set up basic events

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -42,9 +42,9 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
+#define FLAG_PROF_BALL_1    0x20
+#define FLAG_PROF_BALL_2    0x21
+#define FLAG_PROF_BALL_3    0x22
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag

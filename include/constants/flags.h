b556e22335a41941e8ad94d878852efc38b08d0d
jnike01 <bekennedy09@gmail.com>
2024-03-16 16:17:21 -0400

    groudon overworld encounter

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -47,7 +47,7 @@
 #define FLAG_PROF_BALL_3     0x22
 #define FLAG_TEST_BRENDAN    0x23
 #define FLAG_BRENDAN_TRANSITION    0x24
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_OW_GROUDON      0x25
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
